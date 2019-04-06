ceca6297ef6281e2ca4fea51435519b65c37a655
Alejandro <acuellarh@gmail.com>
2019-04-06 09:00:29 -0500

    Bootstrap 4 Added to the project

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,14 @@
+class ArtistsController < ApplicationController
+  def index
+    @prueba = "Hola"
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  root to: "artists#index"
+
+  get 'artists/index'    
+  get 'artists/show'
+  get 'artists/new'
+  get 'artists/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
